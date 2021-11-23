5464d35d9fd1a1045b768fba0aa9308d9ca30fde
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-11-23 11:35:34 +0100

    Add database connector

---
+++ System/DatabaseConnector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace System
+{
+    class DatabaseConnector
+    {
+        public static void Connect()
+        {
+            try 
+            { 
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "127.0.0.1";
+                builder.UserID = "SA";            
+                builder.Password = "Klimaatverandering1";     
+                builder.InitialCatalog = "TestDB";
+
+                using SqlConnection connection = new SqlConnection(builder.ConnectionString);
+                Console.WriteLine("\nQuery data example:");
+                Console.WriteLine("=========================================\n");
+
+                String sql = "SELECT name, quantity FROM Inventory";
+
+                using SqlCommand command = new SqlCommand(sql, connection);
+                connection.Open();
+                
+                using SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetInt32(1));
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
