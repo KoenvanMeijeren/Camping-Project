4a884898cb865fcd1d67cef8703ec0b7f25cabc4
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-11-25 16:08:51 +0100

    Add support for creating reservations

--- Model/Reservation.cs
+++ Model/Reservation.cs
@@ -18,6 +18,11 @@ namespace Model
         public CampingPlace CampingPlace { get; private set; }
         public ReservationDuration Duration { get; private set; }
 
+        public Reservation(string numberOfPeople, CampingCustomer campingCustomer, CampingPlace campingPlace, 
+            ReservationDuration duration): this("0", numberOfPeople, campingCustomer, campingPlace, duration)
+        {
+        }
+        
         public Reservation(string id, string numberOfPeople, CampingCustomer campingCustomer, CampingPlace campingPlace, ReservationDuration duration)
         {
             this.Id = int.Parse(id);
@@ -27,25 +32,24 @@ namespace Model
             this.Duration = duration;
         }
         
-        public Boolean Insert(CampingPlace campingPlace, ReservationDuration reservationDuration)
+        public bool Insert()
         {
             if (this.Id > 0)
             {
                 throw new ArgumentException("You cannot insert an existing reservation.");
-                return false;
             }
             
             Query insertNewReservationQuery = new Query("INSERT INTO Reservation VALUES (@campingPlaceID, @numberOfPeople, @campingCustomerID, @reservationDurationID)");
-            insertNewReservationQuery.AddParameter("campingPlaceID", campingPlace.Id);
+            insertNewReservationQuery.AddParameter("campingPlaceID", this.CampingPlace.Id);
             insertNewReservationQuery.AddParameter("numberOfPeople", this.NumberOfPeople);
             insertNewReservationQuery.AddParameter("campingCustomerID", this.CampingCustomer.Id);
-            insertNewReservationQuery.AddParameter("reservationDurationID", reservationDuration.Id);
+            insertNewReservationQuery.AddParameter("reservationDurationID", this.Duration.Id);
             insertNewReservationQuery.Execute();
             
             return insertNewReservationQuery.SuccessFullyExecuted();
         }
 
-        public Boolean Delete()
+        public bool Delete()
         {
             Query deleteQuery = new Query("DELETE FROM Reservation WHERE id = @id");
             deleteQuery.AddParameter("id", this.Id);
