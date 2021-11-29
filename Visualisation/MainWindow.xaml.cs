f79afbd19558b2a8eaec7b4f3e4b328dfa1cf5d5
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-11-29 11:26:27 +0100

    Debug application start up

--- Visualisation/MainWindow.xaml.cs
+++ Visualisation/MainWindow.xaml.cs
@@ -34,26 +34,27 @@ namespace Visualisation
             this._reservationCollectionFrame = new ReservationCollectionPage(ReservationCollection.Select());
         }
 
-        private void ReserveButtonClick(object sender, RoutedEventArgs e)
+        private void DashboardButtonClick(object sender, RoutedEventArgs e)
         {
-            this.ReserveButton.Background = (SolidColorBrush) new BrushConverter().ConvertFrom("#006837");
-            this.ReserveButton.Foreground = Brushes.White;
+            this.DashboardButton.Background = (SolidColorBrush) new BrushConverter().ConvertFrom("#006837");
+            this.DashboardButton.Foreground = Brushes.White;
 
-            this.OverviewButton.Background = Brushes.White;
-            this.OverviewButton.Foreground = Brushes.Black;
+            this.CampingPitchesButton.Background = Brushes.White;
+            this.CampingPitchesButton.Foreground = Brushes.Black;
+
+            this.MainFrame.Content = this._reservationCollectionFrame.Content;
+        }
+        
+        private void CampingPitchesButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.CampingPitchesButton.Background = (SolidColorBrush) new BrushConverter().ConvertFrom("#006837");
+            this.CampingPitchesButton.Foreground = Brushes.White;
+
+            this.DashboardButton.Background = Brushes.White;
+            this.DashboardButton.Foreground = Brushes.Black;
 
             this.MainFrame.Content = this._campingPitchesCollectionFrame.Content;
         }
 
-        private void OverviewButtonClick(object sender, RoutedEventArgs e)
-        {
-            this.OverviewButton.Background = (SolidColorBrush) new BrushConverter().ConvertFrom("#006837");
-            this.OverviewButton.Foreground = Brushes.White;
-
-            this.ReserveButton.Background = Brushes.White;
-            this.ReserveButton.Foreground = Brushes.Black;
-
-            this.MainFrame.Content = this._reservationCollectionFrame.Content;
-        }
     }
 }
