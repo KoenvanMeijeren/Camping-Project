9b5177f68a476ef1cfce4958aa65639633972d91
Votelesstie <s1157914@student.windesheim.nl>
2021-11-30 16:28:28 +0100

    fixed push

--- Model/ReservationCampingGuest.cs
+++ Model/ReservationCampingGuest.cs
@@ -7,11 +7,21 @@ using System.Threading.Tasks;
 namespace Model
 {
     // todo: henk
-    public class ReservationCampingGuest : IModel
+    public class ReservationCampingGuest : ModelBase<ReservationCampingGuest>
     {
         public int Id { get; private set; }
         public Reservation Reservation { get; private set; }
         public CampingGuest CampingGuest { get; private set; }
+
+        public ReservationCampingGuest()
+        {
+
+        }
+
+        public ReservationCampingGuest(Reservation reservation, CampingGuest campingGuest): this("-1", reservation, campingGuest)
+        {
+
+        }
         
         public ReservationCampingGuest(string id, Reservation reservation, CampingGuest campingGuest)
         {
@@ -19,6 +29,73 @@ namespace Model
             this.Reservation = reservation;
             this.CampingGuest = campingGuest;
         }
-        
+
+        protected override string Table()
+        {
+            return "ReservationCampingGuest";
+        }
+
+        protected override string PrimaryKey()
+        {
+            return "ReservationCampingGuestID";
+        }
+
+        public bool Update(Reservation reservation, CampingGuest campingGuest)
+        {
+            this.Reservation = reservation;
+            this.CampingGuest = campingGuest;
+
+            return base.Update(ReservationCampingGuest.ToDictionary(reservation, campingGuest));
+        }
+
+        protected override ReservationCampingGuest ToModel(Dictionary<string, string> dictionary)
+        {
+            if(dictionary == null)
+            {
+                return null;
+            }
+
+            dictionary.TryGetValue("ReservationCampingGuestID", out string reservationCampingGuestId);
+            dictionary.TryGetValue("ReservationID", out string reservationId);
+            dictionary.TryGetValue("CampingPlaceID", out string campingPlaceID);
+            dictionary.TryGetValue("NumberOfPeople", out string numberOfPeople);
+            dictionary.TryGetValue("CampingCustomerID", out string campingCustomerId);
+            dictionary.TryGetValue("ReservationDurationID", out string reservationDurationId);
+            dictionary.TryGetValue("CampingGuestID", out string campingGuestId);
+            dictionary.TryGetValue("CampingGuestName", out string campingGuestName);
+            dictionary.TryGetValue("Birthdate", out string birthdate);
+
+/*
+            Reservation reservation = new Reservation(reservationId);
+            CampingGuest campingGuest = new CampingGuest(campingGuestId, campingGuestName, Convert.ToDateTime(birthdate));
+
+            return new ReservationCampingGuest(reservation, campingGuest);*/
+
+            return null;
+        }
+        protected override Dictionary<string, string> ToDictionary()
+        {
+            return ReservationCampingGuest.ToDictionary(this.Reservation, this.CampingGuest);
+        }
+
+        private static Dictionary<string, string> ToDictionary(Reservation reservation, CampingGuest campingGuest)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>
+            {
+                { "ReservationID", reservation.Id.ToString()},
+                { "CampingGuestID", campingGuest.Id.ToString()}
+            };
+
+            return dictionary;
+        }
+
+        protected override string BaseQuery()
+        {
+            string query = base.BaseQuery();
+            query += "INNER JOIN Reservation RE ON BT.ReservationID = RE.ReservationID";
+            query += "INNER JOIN CampingGuest CG ON BT.CampingGuestID = CG.CampingGuestID";
+
+            return query;
+        }
     }
 }
