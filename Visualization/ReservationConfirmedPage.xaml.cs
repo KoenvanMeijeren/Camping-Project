e12a423574ff666282343eb9c73995383e2a68ef
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-12-03 16:35:04 +0100

    Refactor code behind of reservation confirmed page to view model

--- Visualization/ReservationConfirmedPage.xaml.cs
+++ Visualization/ReservationConfirmedPage.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ViewModel;
+﻿using System.Windows.Controls;
 
 namespace Visualization
 {
@@ -21,26 +7,9 @@ namespace Visualization
     /// </summary>
     public partial class ReservationConfirmedPage : Page
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime CheckInDatetime { private get; set; }
-        public DateTime CheckOutDatetime { private get; set; }
         public ReservationConfirmedPage()
         {
             this.InitializeComponent();
-            
-            ReservationCustomerFormViewModel.ReservationConfirmedEvent += this.OnReservationConfirmedEvent;
-        }
-
-        private void OnReservationConfirmedEvent(object sender, ReservationEventArgs args)
-        {
-            this.FirstName = args.Reservation.CampingCustomer.FirstName;
-            this.LastName = args.Reservation.CampingCustomer.LastName;
-            this.CheckInDatetime = args.Reservation.Duration.CheckInDatetime;
-            this.CheckOutDatetime = args.Reservation.Duration.CheckOutDatetime;
-
-            this.Title.Content = $"Gefeliciteerd {FirstName} {LastName},";
-            this.ConfirmationText.Content = $"Uw reservering van {this.CheckInDatetime.Date.ToShortDateString()} tot {this.CheckOutDatetime.Date.ToShortDateString()}";
         }
     }
 }
