a366e2c72b60202fefd36478604096365b286486
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-12-06 15:40:45 +0100

    Update view model validation

---
+++ SystemCore/RegexHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SystemCore
+{
+    public class RegexHelper
+    {
+        public static bool IsEmailValid(string email)
+        {
+            var regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", RegexOptions.IgnoreCase);//student@mail.nl
+            return regex.IsMatch(email.Trim());
+        }
+    }
+}
