6009fbea15f861857fb17579e8fe086b8e885f0e
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-12-07 14:17:02 +0100

    Debug select queries

--- Model/ModelBase.cs
+++ Model/ModelBase.cs
@@ -170,7 +170,7 @@ namespace Model
         /// <returns>The query for getting one record from table by ID.</returns>
         protected virtual string SelectQuery()
         {
-            return $"{this.BaseSelectQuery()} WHERE BT.{this.PrimaryKey} = @{this.PrimaryKey}";
+            return $"{this.BaseSelectQuery()} WHERE {this.PrimaryKey} = @{this.PrimaryKey}";
         }
         
         /// <summary>
@@ -180,7 +180,7 @@ namespace Model
         /// <returns>The query for getting the last record from table.</returns>
         protected virtual string SelectLastQuery()
         {
-            return $"{this.BaseSelectQuery()} ORDER BY BT.{this.PrimaryKey} DESC";
+            return $"{this.BaseSelectQuery()} ORDER BY {this.PrimaryKey} DESC";
         }
 
     }
