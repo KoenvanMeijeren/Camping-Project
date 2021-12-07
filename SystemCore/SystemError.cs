510a95115332d4fa907c06ebedb6aa1d70735029
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-12-07 15:30:31 +0100

    Remove unused parameter

--- SystemCore/SystemError.cs
+++ SystemCore/SystemError.cs
@@ -12,8 +12,7 @@ namespace SystemCore
         /// Handles an exception, either shows the error or a user friendly error message and stops running the program.
         /// </summary>
         /// <param name="exception">The exception</param>
-        /// <param name="message">The message.</param>
-        public static void Handle(Exception exception, string message = null)
+        public static void Handle(Exception exception)
         {
             var success = bool.TryParse(ConfigReader.GetSetting("debug"), out bool debug);
             if (success && debug)
