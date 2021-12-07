823977b62d74cef5037873bce52037a6643bc65a
femke <s1154084@student.windesheim.nl>
2021-12-07 12:16:45 +0100

    Busy with data binding ReservationUpdateDeletePage Twoway.

---
+++ ViewModel/ManageReservationViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Model;
+
+namespace ViewModel
+{
+    public class ManageReservationViewModel : ObservableObject
+    {
+        private Reservation _selectedReservation;
+        public Reservation SelectedReservation
+        {
+            get
+            {
+                return this._selectedReservation;
+            }
+            set
+            {
+                if (Equals(value, this._selectedReservation))
+                {
+                    return;
+                }
+
+                this._selectedReservation = value;
+                this.OnPropertyChanged(new PropertyChangedEventArgs(null));
+            }
+        }
+
+        public ManageReservationViewModel()
+        {
+            ReservationCollectionViewModel.ManageReservationEvent += this.OnManageReservationEvent;
+        }
+
+        private void OnManageReservationEvent(object sender, ReservationEventArgs args)
+        {
+            SelectedReservation = args.Reservation;
+        }
+    }
+}
