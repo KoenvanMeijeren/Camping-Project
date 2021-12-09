23147d538773880ad1f22355c166941dd9fb23dd
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2021-12-09 15:51:51 +0100

    Remove console write line

--- ViewModel/ReservationCustomerOverviewViewModel.cs
+++ ViewModel/ReservationCustomerOverviewViewModel.cs
@@ -126,8 +126,6 @@ namespace ViewModel
             this.Reservations = reservationModel.GetCustomersReservations(this._customerID);
             this.ReservationsCollection = new ObservableCollection<Reservation>(this.Reservations);
             this.SelectedReservation = this.Reservations.First();
-
-            Console.WriteLine(this.Reservations);
         }
 
         /// <summary>
