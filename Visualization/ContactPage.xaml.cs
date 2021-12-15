ed4e6182e0ba856688aa8c96c727e8846024bd17
ThiemeWindes <s1147686@student.windesheim.nl>
2021-12-15 14:51:53 +0100

    WIP - adding Google Maps

--- Visualization/ContactPage.xaml.cs
+++ Visualization/ContactPage.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace Visualization
 {
@@ -23,6 +28,35 @@ namespace Visualization
         public ContactPage()
         {
             InitializeComponent();
+
+/*            try
+            {
+                string DistanceApiUrl = ConfigurationManager.AppSettings["DistanceApi"];
+                string myKey = ConfigurationManager.AppSettings["ApiKey"];
+                string url = DistanceApiUrl + txtOrigins.Text + "&destinations=" + txtDestinations.Text"&mode=driving&sensor=false&language=en-EN&units=imperial&Key=" + myKey;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                WebResponse response = request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader sreader = new StreamReader(dataStream);
+                string responsereader = sreader.ReadToEnd();
+                response.Close();
+                DataSet ds = new DataSet();
+                ds.ReadXml(new XmlTextReader(new StringReader(responsereader)));
+                if (ds.Tables.Count > 0)
+                {
+                    if (ds.Tables["element"].Rows[0]["status"].ToString() == "OK")
+                    {
+                        objDistance.durtion = Convert.ToString(ds.Tables["duration"].Rows[0]["text"].ToString().Trim());
+                        objDistance.distance = Convert.ToDouble(ds.Tables["distance"].Rows[0]["text"].ToString().Replace("mi", "").Trim());
+                    }
+                }
+                txtDuration.Text = objDistance.durtion;
+                txtDistance.Text = Convert.ToString(objDistance.distance);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in calculating Distance!" + ex.Message);
+            }*/
         }
     }
 }
