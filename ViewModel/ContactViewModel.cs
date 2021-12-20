46edcc0a07ba831251a79c9a18188363fcb5cef2
ThiemeWindes <s1147686@student.windesheim.nl>
2021-12-20 11:08:02 +0100

    fixed links opening in application + full button is now clickable instead of just the text

--- ViewModel/ContactViewModel.cs
+++ ViewModel/ContactViewModel.cs
@@ -17,6 +17,13 @@ namespace ViewModel
 {
     public class ContactViewModel : ObservableObject
     {
+        #region Fields
+        private int _campingAddressID = 2;
+        private string _facebookLink = "https://www.facebook.com";
+        private string _twitterLink = "https://www.twitter.com";
+        private string _instagramLink = "https://www.instagram.com";
+        #endregion
+
         #region Properties
 
         private string _contactPageAddress = "Adres: ";
@@ -49,13 +56,22 @@ namespace ViewModel
 
         #endregion
 
+        #region Chat button
         public ICommand ChatButton => new RelayCommand(ExecuteGoToChat);
         public static event EventHandler FromContactToChatEvent;
+        #endregion
+
+        #region Social media buttons
+        public ICommand FacebookButton => new RelayCommand<object>((x) => ExecuteLink(_facebookLink));
+        public ICommand InstagramButton => new RelayCommand<object>((x) => ExecuteLink(_instagramLink));
+        public ICommand TwitterButton => new RelayCommand<object>((x) => ExecuteLink(_twitterLink));
+        public static event EventHandler<LinkEventArgs> LinkEvent;
+        #endregion
 
         public ContactViewModel()
         {
             Camping campingModel = new Camping();
-            Camping camping = campingModel.SelectById(2);
+            Camping camping = campingModel.SelectById(_campingAddressID);
 
             this.ContactPageAddress = $"{camping.Address.Street}, {camping.Address.Place}";
             this.ContactPostalCode = camping.Address.PostalCode;
@@ -67,6 +83,17 @@ namespace ViewModel
         private void ExecuteGoToChat()
         {
             FromContactToChatEvent?.Invoke(this, null);
+        }
+        #endregion
+
+        #region Social media
+        /// <summary>
+        /// Event that fires the link to the View
+        /// </summary>
+        /// <param name="href">String of the href you want to visit</param>
+        private void ExecuteLink(string href)
+        {
+            LinkEvent?.Invoke(this, new LinkEventArgs(href));
         }
         #endregion
     }
