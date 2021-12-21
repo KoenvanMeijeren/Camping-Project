3d8ceee98b4fc35665f6d9315fb33c58ab8a5041
ThiemeWindes <s1147686@student.windesheim.nl>
2021-12-21 15:22:04 +0100

    added messageJSON object

---
+++ SystemCore/MessageJSON.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace SystemCore
+{
+
+    /// <summary>
+    /// Role of who sent the message.
+    /// </summary>
+    public enum Role
+    {
+        Customer = 0,
+        Admin = 1
+
+    }
+    public class MessageJSON
+    {
+        public string Message;
+        public DateTime MessageSentTime;
+        public Role UserRole;
+
+        public MessageJSON(string json)
+        {
+            JObject jObject = JObject.Parse(json);
+
+            this.Message = (string)jObject["message"];
+            this.MessageSentTime = DateTimeParser.TryParse((string)jObject["messageSent"]);
+            this.UserRole = (Role)(Convert.ToInt32((string)jObject["role"]));
+        }
+    }
+}
