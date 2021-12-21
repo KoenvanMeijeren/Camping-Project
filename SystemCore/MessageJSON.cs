e6bf55f7635e586d62e39cdc297005ca4e31fdfc
ThiemeWindes <s1147686@student.windesheim.nl>
2021-12-21 16:09:06 +0100

    wip - added json to visible messages in chat

--- SystemCore/MessageJSON.cs
+++ SystemCore/MessageJSON.cs
@@ -3,29 +3,17 @@ using System;
 
 namespace SystemCore
 {
-
-    /// <summary>
-    /// Role of who sent the message.
-    /// </summary>
-    public enum Role
-    {
-        Customer = 0,
-        Admin = 1
-
-    }
     public class MessageJSON
     {
         public string Message;
-        public DateTime MessageSentTime;
-        public Role UserRole;
+        public string MessageSentTime;
+        public string UserRole;
 
-        public MessageJSON(string json)
+        public MessageJSON(string message, string userRole)
         {
-            JObject jObject = JObject.Parse(json);
-
-            this.Message = (string)jObject["message"];
-            this.MessageSentTime = DateTimeParser.TryParse((string)jObject["messageSent"]);
-            this.UserRole = (Role)(Convert.ToInt32((string)jObject["role"]));
+            this.Message = message;
+            this.MessageSentTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            this.UserRole = userRole;
         }
     }
 }
